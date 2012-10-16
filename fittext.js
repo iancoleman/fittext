a826837e9a9a6d7d68e12607a7d2091c6464df67
Ian <ian@ian-dell-laptop.(none)>
2012-10-16 16:43:02 +1100

    Fixed options logic

--- fittext.js
+++ fittext.js
@@ -1,9 +1,15 @@
 /*
- This changes the size of text to fit divs
- which have a fixed width and height.
+
+This changes the size of text to fit element
+that have a fixed width and height.
+ 
  Options include:
  increase: true/false - whether increase in font size is allowed
  decrease: true/false - whether decrease in font size is allowed
+
+ useage as below:
+ $(".mydiv").fittext()
+
  */
 ;(function(window, $)
 {
@@ -16,19 +22,19 @@
                 var MIN_FONT_SIZE_PX = 7,
                     MAX_FONT_SIZE_PX = 100,
                     FONT_ADJUSTMENT_STEP = 1,
-                    decrease = options && options.decrease ? options.decrease : true,
-                    increase = options && options.increase ? options.increase : true;
+                    decrease = options && "decrease" in options ? options.decrease : true,
+                    increase = options && "increase" in options ? options.increase : true;
                     $this = $(this),
-                    width = $this.innerWidth(),
-                    height = $this.innerHeight(),
+                    width = $this.width(),
+                    height = $this.height(),
                     originalFontSize = parseInt($this.css("font-size")),
                     adjustedFontSize = parseInt($this.css("font-size"));
                 var clone = $this.clone();
                 clone.css({
                     "position": "absolute",
                     "display": "block",
-                    "top": $(window).innerHeight(),
-                    "left": $(window).innerWidth(),
+                    "top": $(window).height(),
+                    "left": $(window).width(),
                     "width": width,
                     "whitespace": "normal",
                     "font-size": "",
@@ -37,9 +43,9 @@
                 $("body").append(clone);
 
                 // Work out whether to make the font bigger or smaller
-                var direction = height > clone.innerHeight() ? 1 : -1;
+                var direction = height > clone.height() ? 1 : -1;
                 var limit = direction == -1 ? MIN_FONT_SIZE_PX : MAX_FONT_SIZE_PX;
-                var cloneHeight = clone.innerHeight();
+                var cloneHeight = clone.height();
 
                 if ((decrease && direction == -1) ||
                     (increase && direction ==  1)) {
@@ -48,33 +54,36 @@
                     // This will reduce the number of iterations in the next step.
 
                     // Loop until the font size means the clone height is correct
-                    while ((cloneHeight - height > 0 && direction == -1) || // TODO need to account for max/min font size
-                           (cloneHeight - height < 0 && direction == 1)) { // TODO consider using threshold instead of 0
+                    var has_looped_too_far = false;
+                    while ((cloneHeight - height > 0 && direction == -1 && decrease) || // TODO need to account for max/min font size
+                           (cloneHeight - height < 0 && direction == 1 && increase) &&
+                           !has_looped_too_far) {
                         adjustedFontSize += direction * FONT_ADJUSTMENT_STEP; // TODO make smarter guesses to reduce the number of steps
                         clone.css("fontSize", adjustedFontSize);
-                        cloneHeight = clone.innerHeight();
+                        cloneHeight = clone.height();
+                        var has_looped_too_far =  Math.abs(adjustedFontSize - originalFontSize) < MAX_FONT_SIZE_PX;
                     }
                     if (direction == 1 && adjustedFontSize != originalFontSize) {
                         adjustedFontSize -= FONT_ADJUSTMENT_STEP; // Want the previous size.
                     }
-
-                    // Account for words which are longer than the div width
-                    var content = $this.text();
-                    var words = content.split(/\s/);
-                    clone.css("width", "auto");
-                    $.each(words, function(index, word) {
-                        clone.text(word);
-                        var cloneWidth = clone.innerWidth();
-                        while (cloneWidth - width > 0 && adjustedFontSize > MIN_FONT_SIZE_PX) {
-                            adjustedFontSize -= FONT_ADJUSTMENT_STEP;
-                            clone.css("fontSize", adjustedFontSize);
-                            cloneWidth = clone.innerWidth();
-                        }
-                    });
-
-                    clone.remove();
-                    $this.css("fontSize", adjustedFontSize);
                 }
+
+                // Account for words which are longer than the div width
+                var content = $this.text();
+                var words = content.split(/\s/);
+                clone.css("width", "auto");
+                $.each(words, function(index, word) {
+                    clone.text(word);
+                    var cloneWidth = clone.width();
+                    while (cloneWidth - width > 0 && adjustedFontSize > MIN_FONT_SIZE_PX) {
+                        adjustedFontSize -= FONT_ADJUSTMENT_STEP;
+                        clone.css("fontSize", adjustedFontSize);
+                        cloneWidth = clone.width();
+                    }
+                });
+
+                clone.remove();
+                $this.css("fontSize", adjustedFontSize);
             });
         }
     }
